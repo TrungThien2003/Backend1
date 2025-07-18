056913aefa4d9dbbb66bf4ca4f578fa229736e96
TrungThien2003 <thienb2110146@student.ctu.edu.vn>
2025-07-18 09:22:56 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const contactRouter = require('.app/routes/contact.route');
+const ApiError = require('./app/api-error');
+const contactRouter = require('./app/routes/contact.route');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,5 +11,14 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to book application1' });
     });
 app.use('/api/contacts', contactRouter);
+
+app.use((req, res, next) => {
+   return next(new ApiError(404, 'Resource not found'));
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).json({message: err.message || 'Internal Server Error'});
+});
 module.exports = app;
 
