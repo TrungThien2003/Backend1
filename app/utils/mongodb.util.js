6b27f07a6b907afd096a06eaccde52c9d7b3ca04
TrungThien2003 <thienb2110146@student.ctu.edu.vn>
2025-07-25 09:41:33 +0700

    Revert "Cai dat handler truy xuat CSDL"
    This reverts commit ef7dacd84ae7024740419e4d61935414a36e3dca.

--- app/utils/mongodb.util.js
+++
@@ -1,9 +0,0 @@
-const {MongoClient} = require('mongodb');
-class MongoDb {
-    static connect = async (uri) => {
-        if (this.client) return this.client; 
-        this.client = await MongoClient.connect(uri); 
-        return this.client; 
-    }
-}
-module.exports = MongoDb;
--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 const app = require('./app');
 const config = require('./app/config');
-const MongoDb = require('./app/utils/mongodb.util');
 
-
-async function startServer(){
-    try {
-        await MongoDb.connect(config.db.uri);
-        app.listen(config.app.port, () => {
-            console.log(`Server is running on port ${config.app.port}`);
-        });
-    } catch (error){
-        console.log("Cannot connect to the database!", error);
-        process.exit(); 
-    }
-}
-
-startServer();
+const port = config.app.port; 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
